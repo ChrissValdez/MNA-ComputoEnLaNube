5c06ddbf465e8507e88e9244a302ab0c106db2f7
Christopher Valdez Cantu <30541517+ChrissValdez@users.noreply.github.com>
2023-01-29 20:37:53 -0600

    .

--- SolucionSumaArreglosParalela_Christopher_Valdez_2/SolucionSumaArreglosParalela_Christopher_Valdez_2/SolucionSumaArreglosParalela_Christopher_Valdez_2.cpp
+++ SolucionSumaArreglosParalela_Christopher_Valdez_2/SolucionSumaArreglosParalela_Christopher_Valdez_2/SolucionSumaArreglosParalela_Christopher_Valdez_2.cpp
@@ -2,10 +2,10 @@
 //
 #include <iostream>
 #include <omp.h>
+#include "SolucionSumaArreglosParalela_Christopher_Valdez_2.h"
 
-#define N 1000
 #define chunk 100
-#define mostrar 500
+#define mostrar 100
 
 
 void imprimeArreglo(float* d) {
@@ -17,8 +17,21 @@ void imprimeArreglo(float* d) {
 
 int main()
 {
+    std::cout << "¿De que tamano quieres que sean los arreglos A y B?" << std::endl;
+    int N;
+    std::cin >> N;
+
     std::cout << "Sumando Arreglos en Paralelo!\n";
-    float a[N], b[N], c[N];
+    
+    float *a;
+    a = new float[N];
+    
+    float *b;
+    b = new float[N];
+ 
+    float *c;
+    c = new float[N];
+
     int i;
 
     for (i = 0; i < N; i++) {
@@ -29,7 +42,6 @@ int main()
     int pedazos = chunk;
     
     #pragma omp parallel for shared(a,b,c, pedazos) private(i) schedule(static, pedazos)
-
     for (i = 0; i < N; i++) {
         c[i] = a[i] + b[i];
      }
